928f17293630b6938c930de14858de76c4bec5cf
FIRE <barry7577@gmail.com>
2019-09-17 03:15:18 +0800

    add WPF

---
+++ Wpf_coffeeMaker/Wpf_coffeeMaker/HelperClass.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using Intel.RealSense;
+
+namespace Wpf_coffeeMaker
+{
+    static class HelperClass
+    {
+        public static float GetDistance_3d(this DepthFrame frame, PointF from, PointF to, Intrinsics intr)
+        {
+            // Query the frame for distance
+            // Note: this can be optimized
+            // It is not recommended to issue an API call for each pixel
+            // (since the compiler can't inline these)
+            // However, in this example it is not one of the bottlenecks
+
+            float udist = frame.GetDistance((int)@from.X, (int)@from.Y); //From
+            float vdist = frame.GetDistance((int)to.X, (int)to.Y); //To
+
+            // Deproject from pixel to point in 3D
+            var upoint = DeprojectPixelToPoint(intr, from, udist);
+            var vpoint = DeprojectPixelToPoint(intr, to, vdist);
+
+            // Calculate euclidean distance between the two points
+            return (float)Math.Sqrt(Math.Pow(upoint[0] - vpoint[0], 2) +
+                                     Math.Pow(upoint[1] - vpoint[1], 2) +
+                                     Math.Pow(upoint[2] - vpoint[2], 2));
+        }
+
+        static float[] DeprojectPixelToPoint(Intrinsics intrin, PointF pixel, float depth)
+        {
+            Debug.Assert(intrin.model != Distortion.ModifiedBrownConrady); // Cannot deproject from a forward-distorted image
+            Debug.Assert(intrin.model != Distortion.Ftheta); // Cannot deproject to an ftheta image
+
+            var ret = new float[3];
+            float x = (pixel.X - intrin.ppx) / intrin.fx;
+            float y = (pixel.Y - intrin.ppy) / intrin.fy;
+            if (intrin.model == Distortion.InverseBrownConrady)
+            {
+                float r2 = x * x + y * y;
+                float f = 1 + intrin.coeffs[0] * r2 + intrin.coeffs[1] * r2 * r2 + intrin.coeffs[4] * r2 * r2 * r2;
+                float ux = x * f + 2 * intrin.coeffs[2] * x * y + intrin.coeffs[3] * (r2 + 2 * x * x);
+                float uy = y * f + 2 * intrin.coeffs[3] * x * y + intrin.coeffs[2] * (r2 + 2 * y * y);
+                x = ux;
+                y = uy;
+            }
+            ret[0] = depth * x;
+            ret[1] = depth * y;
+            ret[2] = depth;
+            return ret;
+        }
+
+        static float[] ProjectPointToPixel(Intrinsics intrin, float[] point)
+        {
+            float[] pixel = new float[2];
+            //assert(intrin->model != RS2_DISTORTION_INVERSE_BROWN_CONRADY); // Cannot project to an inverse-distorted image
+
+            float x = point[0] / point[2], y = point[1] / point[2];
+
+            if (intrin.model == Distortion.ModifiedBrownConrady)
+            {
+                float r2 = x * x + y * y;
+                float f = 1 + intrin.coeffs[0] * r2 + intrin.coeffs[1] * r2 * r2 + intrin.coeffs[4] * r2 * r2 * r2;
+                x *= f;
+                y *= f;
+                float dx = x + 2 * intrin.coeffs[2] * x * y + intrin.coeffs[3] * (r2 + 2 * x * x);
+                float dy = y + 2 * intrin.coeffs[3] * x * y + intrin.coeffs[2] * (r2 + 2 * y * y);
+                x = dx;
+                y = dy;
+            }
+            if (intrin.model == Distortion.Ftheta)
+            {
+                float r = (float)Math.Sqrt(x * x + y * y);
+                float rd = (float)(1.0f / intrin.coeffs[0] * Math.Atan((2 * r * Math.Tan(intrin.coeffs[0] / 2.0f))));
+                x *= rd / r;
+                y *= rd / r;
+            }
+
+            pixel[0] = x * intrin.fx + intrin.ppx;
+            pixel[1] = y * intrin.fy + intrin.ppy;
+            return pixel;
+        }
+    }
+}
---
+++ Wpf_coffeeMaker/Wpf_coffeeMaker/MainWindow.xaml.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using Intel.RealSense;
+
+namespace Wpf_coffeeMaker
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CancellationTokenSource _tokenSource = new CancellationTokenSource();
+
+        //Vars for button dragging
+        private Point _dragStartPoint;
+        private bool _dragging = false;
+
+        //Current position of buttons for measuring
+        private System.Drawing.PointF _startPixel;
+        private System.Drawing.PointF _endPixel;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Setup measuring GUI
+                SetStartPos(DragBtn_start);
+                SetEndPos(DragBtn_end);
+
+                //Setup / start camera
+                CameraStart();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Application.Current.Shutdown();
+            }
+        }
+        private void CameraStart()
+        {
+            // Setup filter / alignment settings
+            SetupFilters(out Colorizer colorizer, out DecimationFilter decimate, out SpatialFilter spatial, out TemporalFilter temp, out HoleFillingFilter holeFill, out Align align_to);
+
+            // Setup config settings
+            var cfg = new Config();
+            cfg.EnableStream(Stream.Depth, 1280, 720, Format.Z16, 30);
+            cfg.EnableStream(Stream.Color, 1280, 720, Format.Rgb8, 30);
+
+            // Pipeline start
+            Pipeline pipeline = new Pipeline();
+            PipelineProfile pp = pipeline.Start(cfg);
+
+            // Allocate bitmaps for rendring.
+            // Since the sample aligns the depth frames to the color frames, both of the images will have the color resolution
+            using (var p = pp.GetStream(Stream.Color) as VideoStreamProfile)
+            {
+                Image_show.Source = new WriteableBitmap(p.Width, p.Height, 96d, 96d, PixelFormats.Rgb24, null);
+            }
+            Action<VideoFrame> updateColor = UpdateImage(Image_show);
+
+            //Currently not possible in C#
+            // The hardware can perform hole-filling much better and much more power efficient then our software
+            // auto range = sensor.get_option_range(RS2_OPTION_VISUAL_PRESET);
+            // for (auto i = range.min; i < range.max; i += range.step)
+            //   if (std::string(sensor.get_option_value_description(RS2_OPTION_VISUAL_PRESET, i)) == "High Density")
+            //      sensor.set_option(RS2_OPTION_VISUAL_PRESET, i);
+
+            //var adv = AdvancedDevice.FromDevice(pp.Device);
+            //string path = System.IO.Directory.GetCurrentDirectory();
+            //string full = System.IO.Path.Combine(path, "HighDensityPreset.json");
+            //adv.JsonConfiguration = System.IO.File.ReadAllText(full);
+
+            // Setup frame processing
+            CustomProcessingBlock processingBlock = SetupProcessingBlock(pipeline, colorizer, decimate, spatial, temp, holeFill, align_to);
+
+            // Start frame processing
+            StartProcessingBlock(processingBlock, pp, updateColor, pipeline);
+        }
+
+        private void SetupFilters(out Colorizer colorizer, out DecimationFilter decimate, out SpatialFilter spatial, out TemporalFilter temp, out HoleFillingFilter holeFill, out Align align_to)
+        {
+            // Colorizer is used to visualize depth data
+            colorizer = new Colorizer();
+
+            // Decimation filter reduces the amount of data (while preserving best samples)
+            decimate = new DecimationFilter();
+            decimate.Options[Option.FilterMagnitude].Value = 1.0F;
+
+            // Define spatial filter (edge-preserving)
+            spatial = new SpatialFilter();
+            // Enable hole-filling
+            // Hole filling is an agressive heuristic and it gets the depth wrong many times
+            // However, this demo is not built to handle holes
+            // (the shortest-path will always prefer to "cut" through the holes since they have zero 3D distance)
+            spatial.Options[Option.HolesFill].Value = 5.0F;
+            spatial.Options[Option.FilterMagnitude].Value = 5.0F;
+            spatial.Options[Option.FilterSmoothAlpha].Value = 1.0F;
+            spatial.Options[Option.FilterSmoothDelta].Value = 50.0F;
+
+            // Define temporal filter
+            temp = new TemporalFilter();
+
+            // Define holefill filter
+            holeFill = new HoleFillingFilter();
+
+            // Aline color to depth
+            align_to = new Align(Stream.Depth);
+        }
+
+        private CustomProcessingBlock SetupProcessingBlock(Pipeline pipeline, Colorizer colorizer, DecimationFilter decimate, SpatialFilter spatial, TemporalFilter temp, HoleFillingFilter holeFill, Align align_to)
+        {
+            // Setup / start frame processing
+            CustomProcessingBlock processingBlock = new CustomProcessingBlock((f, src) =>
+            {
+                // We create a FrameReleaser object that would track
+                // all newly allocated .NET frames, and ensure deterministic finalization
+                // at the end of scope. 
+                using (var releaser = new FramesReleaser())
+                {
+                    using (var frames = pipeline.WaitForFrames().DisposeWith(releaser))
+                    {
+                        var processedFrames = frames
+                        .ApplyFilter(align_to).DisposeWith(releaser)
+                        .ApplyFilter(decimate).DisposeWith(releaser)
+                        .ApplyFilter(spatial).DisposeWith(releaser)
+                        .ApplyFilter(temp).DisposeWith(releaser)
+                        .ApplyFilter(holeFill).DisposeWith(releaser)
+                        .ApplyFilter(colorizer).DisposeWith(releaser);
+
+                        // Send it to the next processing stage
+                        src.FramesReady(processedFrames);
+                    }
+                }
+            });
+            return processingBlock;
+        }
+
+        private void StartProcessingBlock(CustomProcessingBlock processingBlock, PipelineProfile pp, Action<VideoFrame> updateColor, Pipeline pipeline)
+        {
+            // Register to results of processing via a callback:
+            processingBlock.Start(f =>
+            {
+                using (var frames = FrameSet.FromFrame(f))
+                {
+                    var color_frame = frames.ColorFrame.DisposeWith(frames);
+                    var depthintr = (pp.GetStream(Stream.Depth) as VideoStreamProfile).GetIntrinsics();
+                    var depth_frame = frames.DepthFrame.DisposeWith(frames);
+
+                    PointConverter pointConverter = new PointConverter();
+
+                    //float k1 = depth_frame.GetDistance_3d(_fromPixel, _toPixel, depthintr);
+                    float distance = HelperClass.GetDistance_3d(depth_frame, _startPixel, _endPixel, depthintr);
+
+                    Dispatcher.Invoke(DispatcherPriority.Render, updateColor, color_frame);
+                    Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(() => DistanceTB.Text = "Distance: " + Math.Round(distance * 100, 2) + " cm"));
+                }
+            });
+
+            var token = _tokenSource.Token;
+
+            var t = Task.Factory.StartNew(() =>
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    using (var frames = pipeline.WaitForFrames())
+                    {
+                        // Invoke custom processing block
+                        processingBlock.ProcessFrames(frames);
+                    }
+                }
+            }, token);
+        }
+
+        static Action<VideoFrame> UpdateImage(Image img)
+        {
+            var wbmp = img.Source as WriteableBitmap;
+            return new Action<VideoFrame>(frame =>
+            {
+                using (frame)
+                {
+                    var rect = new Int32Rect(0, 0, frame.Width, frame.Height);
+                    wbmp.WritePixels(rect, frame.Data, frame.Stride * frame.Height, frame.Stride);
+                }
+            });
+        }
+
+        private void SetStartPos(Button StartButton)
+        {
+            //Calculate button center as start point
+            Point startPoint = StartButton.TranslatePoint(new Point(0, 0), this);
+            startPoint.X = startPoint.X + (StartButton.ActualWidth / 2);
+            startPoint.Y = startPoint.Y + (StartButton.ActualHeight / 2);
+
+            _startPixel = new System.Drawing.PointF((float)startPoint.X, (float)startPoint.Y);
+
+            //Move line object
+            ConnectingLine.X1 = startPoint.X;
+            ConnectingLine.Y1 = startPoint.Y;
+        }
+
+        private void SetEndPos(Button EndButton)
+        {
+            //Calculate button center as end point
+            Point endPoint = EndButton.TranslatePoint(new Point(0, 0), this);
+            endPoint.X = endPoint.X + (EndButton.ActualWidth / 2);
+            endPoint.Y = endPoint.Y + (EndButton.ActualHeight / 2);
+
+            _endPixel = new System.Drawing.PointF((float)endPoint.X, (float)endPoint.Y);
+
+            //Move line object
+            ConnectingLine.X2 = endPoint.X;
+            ConnectingLine.Y2 = endPoint.Y;
+        }
+    }//class
+}//namespace
